a9da601b01bae16ecd0271e8c545c5fe7672ad46
Paulrps <ricardopaulo18@hotmail.com>
2019-03-17 12:12:53 -0300

    commit

--- src/app/players/players.service.ts
+++ src/app/players/players.service.ts
@@ -24,7 +24,7 @@ export class PlayersService {
   getPlayers(): Observable<Player[]> {
     return this.http.get<Player[]>(this.playerUrl)
     .pipe(
-      tap(_ => this.log('fetched players')),
+      tap(_ => this.log('fetched players',_)),
       catchError(this.handleError('getPlayers', []))
     );
   }
@@ -32,14 +32,14 @@ export class PlayersService {
   getPlayerAddData(): Observable<any> {
     return this.http.get<any>(this.playerUrl+"view-add-data")
     .pipe(
-      tap(_ => this.log('fetched view-add-data')),
+      tap(_ => this.log('fetched view-add-data',_)),
       catchError(this.handleError('getPlayerAddData', []))
     );
   }
   
   addPlayer (player: Player): Observable<Player[]> {
     return this.http.post<Player[]>(this.playerUrl, player, httpOptions).pipe(
-      tap((newPlayer: Player[]) => this.log(`added player w/ id=${newPlayer.toString()}`)),
+      tap((newPlayer: Player[]) => this.log(`added player w/ id=${newPlayer.toString()}`,newPlayer)),
       catchError(this.handleError<Player[]>('addPlayer'))
     );
   }
@@ -49,7 +49,7 @@ export class PlayersService {
     const url = `${this.playerUrl}${id}`;
     
     return this.http.delete<Player[]>(url, httpOptions).pipe(
-      tap((newPlayer: Player[]) => this.log(`added player w/ id=${newPlayer.toString()}`)),
+      tap((newPlayer: Player[]) => this.log(`added player w/ id=${newPlayer.toString()}`,newPlayer)),
       catchError(this.handleError<Player[]>('addPlayer'))
     );
   }
@@ -61,14 +61,14 @@ export class PlayersService {
       console.error(error); // log to console instead
       
       // TODO: better job of transforming error for user consumption
-      this.log(`${operation} failed: ${error.message}`);
+      this.log(`${operation} failed: ${error.message}`,error);
       
       // Let the app keep running by returning an empty result.
       return of(result as T);
     };
   }
   
-  private log(message: string) {
-    console.log(message);
+  private log(message: string, object:any) {
+    console.log(message, object);
   }
 }
